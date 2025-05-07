fdbfbcb61bcda171443968c9e0f24ee656e06b2f
tarikulislam <tarikulislam4145@gmail.com>
2025-05-07 18:54:44 +0530

    add server

---
+++ server/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+io.on('connection', (socket) => {
+    console.log('User connected');
+
+    socket.on('offer', (offer) => {
+        socket.broadcast.emit('offer', offer);
+    });
+
+    socket.on('answer', (answer) => {
+        socket.broadcast.emit('answer', answer);
+    });
+
+    socket.on('candidate', (candidate) => {
+        socket.broadcast.emit('candidate', candidate);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+server.listen(5000, () => {
+    console.log('Signaling server running on http://localhost:5000');
+});
